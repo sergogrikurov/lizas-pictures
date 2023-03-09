bbd7eb295b6a34ab06c0be41d6ce319248969d55
sergo grikurov <sergogrikurov@gmail.com>
2023-03-09 19:20:32 +0400

    1

---
+++ js/script.js
@@ -0,0 +1,26 @@
+if (document.querySelector('.lizas__slider')) {
+	new Swiper('.lizas__slider', {
+		pagination: {
+			el: '.controll-main-block__dots',
+			bullets: true,
+			type: 'fraction',
+			renderFraction: function (currentClass, totalClass) {
+				return '0<span class="' + currentClass + '"></span>' +
+					'<span class="slash">/</span>' +
+					'<span class="' + totalClass + '"></span>';
+			},
+		},
+		observer: true,
+		spaceBetween: 20,
+		watchOverflows: true,
+		observeParents: true,
+		speed: 800,
+		loop: true,
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+		}
+	});
+}
+
+
