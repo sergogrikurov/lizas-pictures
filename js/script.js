b435510281530375e6f19aeec21b060ecd949bf3
sergo grikurov <sergogrikurov@gmail.com>
2023-03-09 19:56:08 +0400

    2

--- js/script.js
+++ js/script.js
@@ -22,5 +22,29 @@ if (document.querySelector('.lizas__slider')) {
 		}
 	});
 }
+if (document.querySelector('.lizas__slider2')) {
+	new Swiper('.lizas__slider2', {
+		pagination: {
+			el: '.controll-main-block__dots',
+			bullets: true,
+			type: 'fraction',
+			renderFraction: function (currentClass, totalClass) {
+				return '0<span class="' + currentClass + '"></span>' +
+					'<span class="slash">/</span>' +
+					'<span class="' + totalClass + '"></span>';
+			},
+		},
+		observer: true,
+		spaceBetween: 20,
+		watchOverflows: true,
+		observeParents: true,
+		speed: 800,
+		loop: true,
+		autoplay: {
+			delay: 3000,
+			disableOnInteraction: false,
+		}
+	});
+}
 
 
